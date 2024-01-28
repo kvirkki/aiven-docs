9aa16d98a155bdab455fd60d92009337c91c816d
Dorota Wojcik <dorota.wojcik@aiven.io>
2024-01-28 13:19:15 +0100

    add(postgresql): PGAudit logging

--- sidebars.ts
+++ sidebars.ts
@@ -2015,6 +2015,7 @@ const sidebars: SidebarsConfig = {
                 'products/postgresql/concepts/timescaledb',
                 'products/postgresql/concepts/upgrade-failover',
                 'products/postgresql/concepts/pgvector',
+                'products/postgresql/concepts/pg-audit-logging',
               ],
             },
             {
@@ -2078,6 +2079,18 @@ const sidebars: SidebarsConfig = {
                     'products/postgresql/howto/use-pgvector',
                     'products/postgresql/howto/pg-object-size',
                     'products/postgresql/howto/readonly-user',
+                    {
+                      type: 'category',
+                      label: 'PGAudit',
+                      link: {
+                        type: 'doc',
+                        id: 'products/postgresql/howto/list-pgaudit',
+                      },
+                      items: [
+                        'products/postgresql/concepts/pg-audit-logging',
+                        'products/postgresql/howto/use-pg-audit-logging',
+                      ],
+                    },
                   ],
                 },
                 {
